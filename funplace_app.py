53d8c676701d82d66ecd6c03c3065e6482e7fa10
aduuna <tossia13@gmail.com>
2018-07-25 05:02:54 +0000

    fixed minor bugs in maps direction and api engine

--- funplace_app.py
+++ funplace_app.py
@@ -24,4 +24,4 @@ def result():
 
 if __name__ == '__main__':
     import os
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)), processes=2)
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)), processes=2)
